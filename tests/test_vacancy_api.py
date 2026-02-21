9c1c8a8ed49f4cf5d345f015922bbcaae64a0474
LimDmitriy <160336426+LimDmitriy@users.noreply.github.com>
2026-02-22 01:13:37 +0900

    update(README.md) - Обновлен файл readme

--- tests/test_vacancy_api.py
+++ tests/test_vacancy_api.py
@@ -1,12 +1,10 @@
 from unittest.mock import patch
 
-import pytest
-
 from src.vacancy_api import HeadHunterAPI
 
 
 def test_get_vacancies_from_api():
-    with patch.object(HeadHunterAPI, 'get_vacancies', return_value=[{"name": "Test"}]) as mock_api:
+    with patch.object(HeadHunterAPI, "get_vacancies", return_value=[{"name": "Test"}]) as mock_api:
         api = HeadHunterAPI()
         result = api.get_vacancies("Python")
         assert result == [{"name": "Test"}]
